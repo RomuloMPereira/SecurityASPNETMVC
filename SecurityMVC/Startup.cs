b515b8a21e8ea289bf1d0ac9c791d61004bd5986
Romulo Martins Pereira <mp.romulo@gmail.com>
2021-11-19 20:02:51 -0300

    Create ASP.NET MVC Web Application with Authentication

---
+++ SecurityMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SecurityMVC.Startup))]
+namespace SecurityMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
